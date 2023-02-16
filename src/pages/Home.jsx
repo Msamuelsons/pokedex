b38972710713c16484f04aca43f3f376522056f1
Msamuelsons <marccossamuel@hotmail.com>
2023-02-15 22:16:13 -0300

    finalizado

---
+++ src/pages/Home.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import { Container } from "@mui/system";
+import NavBar from "../components/NavBar";
+import PokemonCard from "../components/PokemonCard";
+import { Grid } from "@mui/material";
+import axios from "axios";
+export const Home = () => {
+  const [pokemons, setPokemon] = useState([]);
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
+  const getPokemons = () => {
+    var endpoints = [];
+    for (var i = 1; i < 50; i++) {
+      endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}`);
+    }
+    axios
+      .all(endpoints.map((endpoint) => axios.get(endpoint)))
+      .then((res) => setPokemon(res));
+  };
+
+  const pokemonFilter = (name) => {
+    var filterPokemons = [];
+    if (name === "") {
+      getPokemons();
+    }
+    for (var i in pokemons) {
+      if (pokemons[i].data.name.includes(name)) {
+        filterPokemons.push(pokemons[i]);
+      }
+    }
+    setPokemon(filterPokemons);
+  };
+  return (
+    <div>
+      <NavBar pokemonFilter={pokemonFilter} />
+      <Container maxWidth="false">
+        <Grid container spacing={3}>
+          {pokemons.map((pokemon, key) => (
+            <Grid item xs={2} key={key}>
+              <PokemonCard
+                name={pokemon.data.name}
+                image={pokemon.data.sprites.front_default}
+                types={pokemon.data.types}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </div>
+  );
+};
